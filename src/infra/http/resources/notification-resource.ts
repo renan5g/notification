77c2add038d3ca6d40bd586475372e25bfd4fe56
renan5g <renansilva.dev5g@gmail.com>
2023-05-19 10:40:49 -0300

    feat: add notification resource and request

---
+++ src/infra/http/resources/notification-resource.ts
@@ -0,0 +1,12 @@
+import { Notification } from '@domain/entities';
+
+export class NotificationResource {
+  static toHTTP(notification: Notification) {
+    return {
+      id: notification.id,
+      content: notification.content.value,
+      category: notification.category,
+      recipientId: notification.recipientId,
+    };
+  }
+}
