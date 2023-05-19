c23c9d2bf2045f374a4eaa725f74136b177c817e
renan5g <renansilva.dev5g@gmail.com>
2023-05-19 09:22:38 -0300

    feat: notification factory

---
+++ test/factories/notification.ts
@@ -0,0 +1,16 @@
+import { Content } from '@domain/notification/content';
+import {
+  Notification,
+  NotificationProps,
+} from '@domain/notification/notification';
+
+type Override = Partial<NotificationProps>;
+
+export function makeNotification(override: Override = {}) {
+  return new Notification({
+    category: 'social',
+    content: new Content('Nova solicitação de amizade!'),
+    recipientId: 'recipient-2',
+    ...override,
+  });
+}
